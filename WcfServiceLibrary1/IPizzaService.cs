2b933bec03d634bdf10d222660b6f9399b2f72bf
Daphne Hogeterp <daphne_hogeterp@hotmail.com>
2018-05-27 20:48:29 +0200

    added gitignore + database

---
+++ WcfServiceLibrary1/IPizzaService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IPizzaService" in both code and config file together.
+    [ServiceContract]
+    public interface IPizzaService
+    {
+        [OperationContract]
+        String Register(string username);
+    }
+}
---
+++ WcfServiceLibrary1/PizzaService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "PizzaService" in both code and config file together.
+    public class PizzaService : IPizzaService
+    {
+        public string Register(string username)
+        {
+            char[] charArray = username.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public void Login(string username, string password)
+        {
+
+        }
+    }
+}

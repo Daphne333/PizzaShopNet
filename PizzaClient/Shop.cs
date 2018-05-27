a7763fbb29082da206fae0ae28f26cb9595e75cb
Daphne Hogeterp <daphne_hogeterp@hotmail.com>
2018-05-27 21:08:11 +0200

    added forms

--- PizzaClient/Shop.cs
+++ PizzaClient/Shop.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace PizzaClient
 {
-    public partial class Form1 : Form
+    public partial class Shop : Form
     {
-        public Form1()
+        public Shop()
         {
             InitializeComponent();
         }

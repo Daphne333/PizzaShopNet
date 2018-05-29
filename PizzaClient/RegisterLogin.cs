8a9de9a8c6ae3d203be485cf334752c13703f650
Daphne Hogeterp <daphne_hogeterp@hotmail.com>
2018-05-29 22:33:30 +0200

    some gui wpf

--- PizzaClient/RegisterLogin.cs
+++ PizzaClient/RegisterLogin.cs
@@ -8,7 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using PizzaClient.PizzaReference;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace PizzaClient
 {
--- PizzaClient/Shop.cs
+++ PizzaClient/Shop.cs
@@ -7,14 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PizzaClient.PizzaReference;
 
 namespace PizzaClient
 {
     public partial class Shop : Form
     {
+
+        PizzaServiceClient pizzaProxy = new PizzaServiceClient();
+
         public Shop()
         {
             InitializeComponent();
+
+            PizzaLijstProducts = pizzaProxy.ProductList();
         }
 
         private void Inventory_Click(object sender, EventArgs e)
